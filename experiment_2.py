d4c3562bc78d19fde2c387904f6b43fad76b09ba
392781 <rlencevicius@gmail.com>
2019-07-30 23:58:51 -0700

    Added/fixed the experiments

--- experiment_2.py
+++ experiment_2.py
@@ -9,13 +9,17 @@ from tqdm import tqdm
     each one
 """
 
-input_path = glob.glob('./faces/input/*.*')
+input_path = glob.glob('./faces/input_tests/*.*')
 target_path = glob.glob('./faces/target/*.*')
-input_test_path = glob.glob('./faces/input_tests/*.*')
+mask_path = glob.glob('./results/experiment_1/delta/*.*')
+input_mask_path = glob.glob('./results/experiment_1/input/*.*')
+input_test_path = glob.glob('./faces/input/*.*')
 target_test_path = glob.glob('./faces/target_tests/*.*')
 
 input_path.sort()
 target_path.sort()
+mask_path.sort()
+input_mask_path.sort()
 input_test_path.sort()
 target_test_path.sort()
 
@@ -34,15 +38,16 @@ for image_path in tqdm(target_path):
     target_list.append(att.detect_face(image_path))
 
 print('\nMASKS ----------')
-for face, _ in tqdm(input_list):
-    mask_list.append(att.create_mask(face))
+for image_path in tqdm(mask_path):
+
+    mask_list.append(att.fr.load_image_file(image_path))
 
 print('\nMASK OFFSET ----')
 for i in tqdm(range(len(mask_list))):
-    mask_list[i] = att.mask_offset(input_list[i], mask_list[i][0], mask_list[i][1])
+    coor = att.detect_face(input_mask_path[i])[1]
+    mask_list[i] = att.mask_offset(input_list[i], mask_list[i], coor)
     
 attack = att.Attack(input_list, target_list, mask_list, 'adamax')
-attack.train(epochs = 45)
 
 for image_path in input_test_path:
     input_test_list.append(att.detect_face(image_path)[0])
@@ -50,4 +55,4 @@ for image_path in input_test_path:
 for image_path in target_test_path:
     target_test_list.append(att.detect_face(image_path)[0])
 
-attack.results(input_test_list, target_test_list, '/experiment_1/')
+attack.results(input_test_list, target_test_list, '/experiment_2/')
