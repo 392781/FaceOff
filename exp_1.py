0b7fab22dc83d8786c1d6abb9451a226adf9eb36
392781 <rlencevicius@gmail.com>
2019-10-22 07:55:42 -0700

    comments

--- exp_1.py
+++ exp_1.py
@@ -3,19 +3,34 @@ import gc
 from copy import deepcopy
 from adversarial_face_recognition import *
 
+# Opens the database
+# Files are stored as a list of tuples.
+# 
+# [(ID_Number, embedding_1, embedding_2, embedding_3, 
+#   embedding_4, embedding_5, Avg_embedding), 
+#   ...
+# ]
 with open('./database.file', 'rb') as f:
     database = pickle.load(f)
 
+# Loads training image
 print('Loading data')
 input_image = load_data('./faces/input/ronald.jpg')[0]
 
-
+# Use GPU if available.
 device = t.device('cuda:0' if t.cuda.is_available() else 'cpu')
 
+# Tools ----------
+
+# Image normalization using ImageNet normalization values
 norm = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 norm = norm.to(device)
+
+# Used to apply a mask onto an image
 apply = Applier()
 apply = apply.to(device)
+
+# Model
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 resnet = resnet.to(device)
 
@@ -24,10 +39,13 @@ try:
 except:
     print('No cuda :(')
 
+# 1 image to train on for every target
 input_list = []
 for i in range(0, 5):
     input_list.append(input_image)
 
+# Creating embeddings of ground truths for loss function
+# Creating tensors of ground truth images for training with masks
 print('Creating inputs\n')
 input_emb = []
 input_tensors = []
@@ -35,48 +53,70 @@ for image, _ in input_list:
     input_emb.append(resnet(norm(tensorize(image))))
     input_tensors.append(tensorize(image))
 
+# Masks for each input image that will train...
+# So in this case 5 masks, for 5 target images of 1 identity
 mask_list = []
 for face, _ in input_list:
     mask_list.append(create_mask(face))
 
+# Offset the mask... Not important, was made to deal with a problem that doesn't 
+# happen often
 for j in range(len(mask_list)):
     mask_list[j] = mask_offset(input_list[j], mask_list[j][0], mask_list[j][1])
 
+# TRAINING
 epochs = 40
 for i in range(0, len(database)):
+    # For each identity in the database, reset the mask list
     list_of_masks = deepcopy(mask_list)
     opt = optim.Adamax(list_of_masks, lr = 1e-1, weight_decay = 0.0001)
+    # Initialize the target images of identity 'i'
     target_emb = database[i][1:6]
 
+    # Necessary lists for training procedure
     adversarial_list = [None for v in range(len(list_of_masks))]
     embeddings = [None for v in range(len(input_tensors))]
     losses = [None for v in range(len(input_tensors))]
 
+    # Save locations
     save_emb_loc = './results/exp_1/id_' + str(database[i][0]) + '_emb.file'
     save_masks_loc = './results/exp_1/id_' + str(database[i][0]) + '_masks.file'
 
-    # Begin training
+    # Begin training 40 epochs on 5 images of 1 identity
     for k in tqdm(range(epochs), desc=f'IMG #: {i+1:2}'):
         for h in tqdm(range(len(adversarial_list)), desc=f'ID: {database[i][0]:5}'):
 
+            # Apply the mask overlay onto the image
             adversarial_list[h] = apply(input_tensors[h], list_of_masks[h])
+            # Calculate the adversarial embedding
             embeddings[h] = resnet(norm(adversarial_list[h]))
 
+            # Loss function -> Maximize distance between input_emb (ground truth)
+            #                  Minimize distance between target_emb
             losses[h] = (-emb_distance(embeddings[h], input_emb[h])
                                 +emb_distance(embeddings[h], target_emb[h]))
             
+            # Backpropagation
             losses[h].backward(retain_graph=True)
             opt.step()
             opt.zero_grad()
 
+            # Keep the mask pixels within a certain range
             list_of_masks[h].data.clamp_(-1, 1)
+    
+    # Prepare for saving...
+    # list_of_masks and embeddings already have all the images
+    # Just need to add the identity of the images for when we gather results
     list_of_masks.insert(0, database[i][0])
     embeddings.insert(0, database[i][0])
+    # Save!
     with open(save_emb_loc, 'wb') as f:
         pickle.dump(embeddings, f)
     with open(save_masks_loc, 'wb') as f:
         pickle.dump(list_of_masks, f)
 
+    # strongly worded letter to the interpreter to clean up :)
+    # I was having memory issues, this was just to be sure everything was cleaned up
     del list_of_masks
     del adversarial_list
     del embeddings
@@ -84,11 +124,4 @@ for i in range(0, len(database)):
     del target_emb
     del opt
     gc.collect()
-
-
-        
-
-        # NEED TO SAVE ALL THE ADVERSARIAL LISTS
-        # COMPARE EACH TO THE DATABASEEEEEEEEEEE
-        # FIND THE CLOSEST IDENTITY 
 
--- results.py
+++ results.py
@@ -6,11 +6,17 @@ from adversarial_face_recognition import *
 with open('./database.file', 'rb') as f:
     database = pickle.load(f)
 
+# Image normalization using ImageNet normalization values
 norm = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# Model
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
+# Original image before adversarial perturbation 
 ground_truth_emb = resnet(norm(tensorize(load_data('./faces/input/ronald.jpg')[0][0])))
 
+# This is used to recombine all the saved tuples into a list of tuples which are in the form of:
+# (ID_Number, embedding_1, embedding_2, embedding_3, 
+#  embedding_4, embedding_5)
 emb_loc = glob.glob('./results/exp_1/embeddings/*')
 emb_loc.sort()
 adv_list = []
@@ -18,20 +24,35 @@ for loc in emb_loc:
     with open(loc, 'rb') as f:
         adv_list.append(pickle.load(f))
 
+# Data gathering
+# counter is for the total number of images
 counter = 0
+# t_count is for the total number of images that were true
 t_count = 0
+
 for adv_img_list in tqdm(adv_list):
     for img_idx in range(1, 6):
+        # Initialize the base case of the closest ID
         closest_id = None
         closest_dist = emb_distance(ground_truth_emb, adv_img_list[img_idx])
+        
+        # Within this identity in the database
         for db_list in database:
+            # Search the 5 images of that identity
             for id_img_idx in range(1, 6):
+                # For the distance
                 dist = emb_distance(adv_img_list[img_idx], db_list[id_img_idx])
+
+                # If the distance is smaller than the base case
+                # then it becomes the new closest ID
                 if (dist < closest_dist):
                     closest_id = db_list[0]
                     closest_dist = dist
+
+    # Checking if the closest ID is the target ID
     if (closest_id == adv_img_list[0]): 
         t_count += 1
     counter += 1
 
+# Outputs!
 print(t_count, counter, t_count/counter)
