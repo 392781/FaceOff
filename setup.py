2d33e83b6b166650f5d9193d1e56c9ac738b026d
392781 <rlencevicius@gmail.com>
2021-02-22 23:00:15 -0800

    now installable through pip! (not on PyPi yet)

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
-# TO-DO : sometime...
+from setuptools import setup
+
+setup(
+    name='FaceOff',
+    version='0.0.1',    
+    description='Steps towards physical adversarial attacks on facial recognition',
+    url='https://github.com/392781/FaceOff',
+    author='Ronald Lencevičius',
+    license='GPL 3.0',
+    packages=['FaceOff'],
+    install_requires=['torch==1.7.1',
+                      'torchvision==0.8.2',
+                      'facenet-pytorch==2.5.1',
+                      'face_recognition',
+                      'cmake',
+                      'dlib',
+                      'numpy',
+                      'Pillow'
+                      ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+    ],
+)
