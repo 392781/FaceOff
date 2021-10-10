d1712625c0a81ff2a1115c3bfcaddcedcf97b974
392781 <rlencevicius@gmail.com>
2021-10-09 18:35:50 -0700

    removed unnecessary examples and created a new example usage

--- examples/example.py
+++ examples/example.py
@@ -1,28 +1,15 @@
-from adversarial_face_recognition import *
+from FaceOff.AFR import load_data, Attack
+from PIL import Image
 
 # Load the data.  This will detect and resize the faces
-input_list = load_data('./faces/input/*.*')
-target_list = load_data('./faces/target/*.*')
+inputs = load_data('./faces/input/')
+targets = load_data('./faces/target/')
 
-# A list for calculated face masks
-mask_list = []
+# Initialize the Attack object with 
+adversarial = Attack(inputs[0], targets[3], optimizer='adam')
 
-# Create the masks for the inputs
-for face, _ in tqdm(input_list):
-    mask_list.append(create_mask(face))
+# Perform adversarial training
+adversarial_tensor, mask_tensor, img = adversarial.train(detect=True, verbose=True)
 
-# If training on differing masks and inputs (i.e. mask ID doesn't match input ID)
-# offset the mask
-for i in tqdm(range(len(mask_list))):
-    mask_list[i] = mask_offset(input_list[i], mask_list[i][0], mask_list[i][1])
-
-# Create attack class and train using adamax
-attacker = Attack(input_list, target_list, mask_list, 'adamax')
-attacker.train(epochs=45)
-
-# For result testing load the image testing set
-input_test_list = load_data('./faces/input_tests/*.*')
-target_test_list = load_data('./faces/target_tests/*.*')
-
-# Print your results.  Make sure you've created the save directory beforehand
-attacker.results(input_test_list, target_test_list, '/results/')
+# Show the image with mask applied
+img.show()
